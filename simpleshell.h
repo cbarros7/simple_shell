a550954cede58a448ede48270b793c7083013b3e
AndrewKalil <1541@holbertonschool.com>
2020-04-14 17:16:12 -0500

    packed header

--- simpleshell.h
+++ simpleshell.h
@@ -1,6 +1,5 @@
-#ifndef SIMPLESHELL_H
-#define SIMPLESHELL_H
-
+#ifndef _SIMPLESHELL_H_
+#define _SIMPLESHELL_H_
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -11,34 +10,26 @@
 #include <fcntl.h>
 #define TOK_BUFSIZE 64
 #define TOK_DELIM " \t\r\n\a"
-
 extern char **environ;
-
 void handle_signal(int signal);
 int _stat(char *s);
-
 int func_exit(char **args);
 int func_printenv(char **args);
-
 char *_which(char *args);
 char *_getenv(char *path);
 char *search_func(char **dir, char *cmd);
 int child_process(char **args, char **argv, int count);
 int permission(char *filename);
-
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
 char *_strstr(char *str1, char *str2);
 char *_strdup(char *str);
-
 char *read_input(void);
 char **sparse_str(char *line);
 int execute(char **args, char **argv, int count);
 void prompt(void);
-
 void *_realloc(void *ptr, unsigned int new_size);
 int _count_point(char *buffer);
-
 #endif
